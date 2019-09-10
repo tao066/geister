20d8326e96948b9fa0cd566619843f980c2aba7d
ktsuchiya <justanothercommoner2989@gmail.com>
2019-09-10 12:49:59 +0900

    [create] User logout and refactoring title page

---
+++ Assets/Utility/DeleteUserSessionDirector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+using HTTP;
+using Protocol;
+
+public class DeleteUserSessionDirector : MonoBehaviour {
+
+	public void DeleteUserSession() {
+        ApiClient.Instance.SetIpAddress("http://127.0.0.1:3000");
+
+        RequestDeleteUserSession param = new RequestDeleteUserSession();
+
+        param.user_session_id = PlayerSession.user_session_id;
+
+        ApiClient.Instance.SetAccessToken(PlayerSession.access_token);
+        ApiClient.Instance.ResponseDeleteUserSession = ResponseDeleteUserSession;
+        ApiClient.Instance.RequestDeleteUserSession(param);
+    }
+	
+	public void ResponseDeleteUserSession(ResponseDeleteUserSession response) {
+        PlayerSession.user_session_id = 0;
+        PlayerSession.access_token = "";
+        PlayerSession.user_id = 0;
+
+        Debug.Log("Logout: " + response.name);
+
+        SceneManager.LoadScene("title_scene");
+    }
+}
